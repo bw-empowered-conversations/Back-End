6328e75ed22f5b947daea2cfb3cd960ebb1312ab
Kronykus <MoreheadSteve@gmail.com>
2019-12-22 15:17:50 -0600

    additions

--- api/middleware-config.js
+++ api/middleware-config.js
@@ -1,11 +1,14 @@
+require( 'dotenv' ).config();
 const express = require( 'express' );
 const helmet  = require( 'helmet'  );
+const morgan  = require( 'morgan'  );
 const cors    = require( 'cors'    );
 
 const knex = require( '../data/dbConfig' );
 
 module.exports = server => {
-  server.use( helmet()       );
-  server.use( express.json() );
-  server.use( cors()         );
+  server.use( morgan( 'dev' ) );
+  server.use( helmet()        );
+  server.use( express.json()  );
+  server.use( cors()          );
 };
